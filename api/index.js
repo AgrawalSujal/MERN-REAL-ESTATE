33c2d526b20b2881b1558d482c6f26defaaa09fa
AgrawalSujal <agrawalsujal6522@gmail.com>
2024-08-23 00:31:58 +0530

    Connecting to database (MONGODB)

--- api/index.js
+++ api/index.js
@@ -10,10 +10,10 @@ mongoose
   })
   .catch((err) => {
     console.log(`Some error occured connecting to Mongodb`, err);
+    console.log(err);
   });
 const app = express();
 
 app.listen(3000, () => {
   console.log("Sever is listening at 3000");
-  console.log(err);
 });
