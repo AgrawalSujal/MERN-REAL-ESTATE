d616fc5910b84d7d8aacf18cde5b5b6aeb92fba7
AgrawalSujal <agrawalsujal6522@gmail.com>
2024-08-22 23:51:28 +0530

    Creating Componenets and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import About from "./Pages/About";
+import Search from "./Pages/Search";
+import Listing from "./Pages/Listing";
+import Profile from "./Pages/Profile";
+import CreateListing from "./Pages/CreateListing";
+import UpdateListing from "./Pages/UpdateListing";
+import PrivateRoute from "./Components/PrivateRoute.jsx";
+import Header from "./Components/Header.jsx";
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline bg-red-400">Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/Pages/CreateListing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CreateListing = () => {
+  return (
+    <div>
+      <h1>CreateListing Page</h1>
+    </div>
+  );
+};
+
+export default CreateListing;
