169af8016da9520c78fc0fc723dc3867f877485a
AgrawalSujal <agrawalsujal6522@gmail.com>
2024-08-23 00:29:58 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`Connected to Mongodb successfully`);
+  })
+  .catch((err) => {
+    console.log(`Some error occured connecting to Mongodb`, err);
+  });
 const app = express();
 
 app.listen(3000, () => {
   console.log("Sever is listening at 3000");
+  console.log(err);
 });
